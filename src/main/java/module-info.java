8638a0728f9d9d1766e9efdcb619b2a035c9cce1
Gabrielle Brandt <110957615+brandt47@users.noreply.github.com>
2024-11-21 17:49:14 -0700

    added arcGIS dependecies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module ca.macewan.cmpt305.propertyassessmentapplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
+    requires javafx.web;
+    requires com.esri.arcgisruntime;
 
 
     opens ca.macewan.cmpt305.propertyassessmentapplication to javafx.fxml;
