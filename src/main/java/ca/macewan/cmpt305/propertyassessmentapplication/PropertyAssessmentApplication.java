e114775848efc127ecd87fb9a82f16c40f219a55
Gabrielle Brandt <110957615+brandt47@users.noreply.github.com>
2024-11-23 17:38:37 -0700

    added one point

--- src/main/java/ca/macewan/cmpt305/propertyassessmentapplication/PropertyAssessmentApplication.java
+++ src/main/java/ca/macewan/cmpt305/propertyassessmentapplication/PropertyAssessmentApplication.java
@@ -3,9 +3,15 @@ package ca.macewan.cmpt305.propertyassessmentapplication;
 // This is a test comment
 
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+//import com.esri.arcgisruntime.internal.jni.CoreGeoView;
 import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,9 +19,11 @@ import javafx.scene.SubScene;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
+//import java.awt.*;
 import java.io.IOException;
 
 
@@ -59,10 +67,29 @@ public class PropertyAssessmentApplication extends Application {
         mapView.prefWidthProperty().bind(mapSubScene.widthProperty());
         mapView.prefHeightProperty().bind(mapSubScene.heightProperty());
 
+        // Add a GraphicsOverlay to the MapView
+        GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
+        mapView.getGraphicsOverlays().add(graphicsOverlay);
+
+
+        //https://developers.arcgis.com/java/maps-2d/add-graphics-to-a-map-view/
+        // Create a point for MacEwan
+        Point macewanPoint = new Point(-113.5076, 53.5471, SpatialReferences.getWgs84());
+
+        // Create a red circle symbol for the point
+        SimpleMarkerSymbol redDiamondSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.DIAMOND, Color.RED, 15);
+
+        // Create a graphic for the point
+        Graphic macewanGraphic = new Graphic(macewanPoint, redDiamondSymbol);
+
+        // Add the graphic to the graphics overlay
+        graphicsOverlay.getGraphics().add(macewanGraphic);
+
         // Show the stage
         stage.setTitle("Property Assessment Application");
         stage.setScene(scene);
         stage.show();
+
     }
 
 
